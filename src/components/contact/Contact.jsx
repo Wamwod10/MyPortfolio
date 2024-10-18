e6d9ed61dbf9509965b9ec87dac7cb963462b310
Wamwod <shamshodochilov160@gmail.com>
2024-10-18 18:00:42 +0500

    style

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,8 +6,11 @@ const Contact = () => {
     <div className="contact">
         <div className="container">
             <div className="contact__box">
-                <div className="contact__box-first"></div>
-                <div className="contact__box-second"></div>
+                <div className="contact__box-first">
+                    <h2 className="contact__title">Send a Message</h2>
+                    <p className="contact__txt">We look forward to your thoughts. Please fill out the fields below and send your message.</p>
+                    <input type="text" className='contact__input-1' placeholder='Enter your Name'/>
+                </div>
             </div>
         </div>
     </div>
